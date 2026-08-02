b4b72580ae75ab50a141f9712fd15e366efac368
Tuyen <tuyenctbk@gmail.com>
2026-08-02 08:31:05 +0700

    feat: add Toast import to UpdateHelper for user feedback

--- app/src/main/java/com/tdpham/navitvbrowser/util/UpdateHelper.kt
+++ app/src/main/java/com/tdpham/navitvbrowser/util/UpdateHelper.kt
@@ -3,6 +3,7 @@ package com.tdpham.navitvbrowser.util
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.core.content.edit
 import androidx.core.net.toUri
